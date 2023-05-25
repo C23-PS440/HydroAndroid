2eb3db83c9570b46fbe6a803b19248d484cab404
PoconGGaul <romuty16@gmail.com>
2023-05-25 22:32:57 +0700

    Register Cek API :stuck_out_tongue_closed_eyes:

--- app/src/main/java/com/capstone/hydroandroid/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/capstone/hydroandroid/ui/register/RegisterFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.capstone.hydroandroid.data.network.EventResult
+import com.capstone.hydroandroid.data.network.request.LoginRequest
+import com.capstone.hydroandroid.data.network.request.RegisterRequest
 import com.capstone.hydroandroid.databinding.FragmentRegisterBinding
+import com.capstone.hydroandroid.ui.login.LoginViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class RegisterFragment : Fragment() {
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: RegisterViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,8 +25,27 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val data = RegisterRequest(
+            email = "", password = "", name = ""
+        )
+
+        viewModel.register(data).observe(viewLifecycleOwner) {
+            when (it) {
+                is EventResult.Error -> {
+                    Toast.makeText(requireContext(), it.error, Toast.LENGTH_SHORT).show()
+                }
+                is EventResult.Loading -> {
+                }
+                is EventResult.Success -> {
+                    Toast.makeText(requireContext(), it.data.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
