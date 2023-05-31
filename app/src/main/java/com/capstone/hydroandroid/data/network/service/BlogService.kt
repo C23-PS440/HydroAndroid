e0bd8b38c0c2862ea17b4727357e63637db420ba
cyncindy <cindydevonline@gmail.com>
2023-06-01 01:33:55 +0800

    Search Features :rabbit2:

--- app/src/main/java/com/capstone/hydroandroid/data/network/service/BlogService.kt
+++ app/src/main/java/com/capstone/hydroandroid/data/network/service/BlogService.kt
@@ -2,9 +2,11 @@ package com.capstone.hydroandroid.data.network.service
 
 import com.capstone.hydroandroid.data.network.response.detail.DetailResponse
 import com.capstone.hydroandroid.data.network.response.home.HomeResponse
+import com.capstone.hydroandroid.data.network.response.search.SearchResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BlogService {
     //GET ALL BLOG
@@ -15,4 +17,9 @@ interface BlogService {
     suspend fun getDetailBlog(
         @Path("blogId") blogId : String
     ) : Response<DetailResponse>
+
+    @GET("search/blog")
+    suspend fun getSearchedBlog(
+        @Query("query") query : String
+    ) : Response<SearchResponse>
 }
