ed6d6a38e0bccb01bd112920c811231d2eb90cc6
PoconGGaul <romuty16@gmail.com>
2023-06-03 15:25:47 +0700

    Fab Bottom

--- app/src/main/java/com/capstone/hydroandroid/ui/MainActivity.kt
+++ app/src/main/java/com/capstone/hydroandroid/ui/MainActivity.kt
@@ -19,13 +19,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        val navView : BottomNavigationView =  binding.navView
+        navView.background = null
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navController.addOnDestinationChangedListener{_,destination,_ ->
             when(destination.id){
                 R.id.navigation_home -> showButtonNav()
                 R.id.navigation_profile -> showButtonNav()
-                R.id.navigation_camera -> {
+                R.id.navigation_blog -> {
+
+                }
+                R.id.place_holder -> navView.menu.getItem(2).isEnabled = false
+                R.id.fab -> {
                     Toast.makeText(this,"Ini Camera" , Toast.LENGTH_SHORT).show()
                 }
                 else -> hideButtonNav()
@@ -34,12 +39,19 @@ class MainActivity : AppCompatActivity() {
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_camera, R.id.navigation_profile
+                R.id.navigation_home, R.id.navigation_blog, R.id.navigation_profile
             )
         )
 //        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
-    private fun showButtonNav() {binding.navView.visibility = View.VISIBLE}
-    private fun hideButtonNav() {binding.navView.visibility = View.GONE}
+    private fun showButtonNav() {
+        binding.navView.visibility = View.VISIBLE
+
+    }
+    private fun hideButtonNav() {
+        binding.navBar.visibility = View.GONE
+        binding.fab.visibility = View.GONE
+        binding.navView.visibility = View.GONE
+    }
 }
