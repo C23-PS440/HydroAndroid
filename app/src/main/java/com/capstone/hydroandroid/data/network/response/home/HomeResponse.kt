bfe9c37d2ec3bcd5347b325197d8def768ecad82
PoconGGaul <romuty16@gmail.com>
2023-06-06 21:26:04 +0700

    Blog User GET API

--- app/src/main/java/com/capstone/hydroandroid/data/network/response/home/HomeResponse.kt
+++ app/src/main/java/com/capstone/hydroandroid/data/network/response/home/HomeResponse.kt
@@ -1,13 +1,39 @@
 package com.capstone.hydroandroid.data.network.response.home
 
-
 import com.google.gson.annotations.SerializedName
 
 data class HomeResponse(
-    @SerializedName("blog")
-    val blog: List<Blog>,
-    @SerializedName("error")
-    val error: Boolean,
-    @SerializedName("message")
-    val message: String
+
+	@field:SerializedName("blogs")
+	val blogs: List<BlogsItem>,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class BlogsItem(
+
+	@field:SerializedName("blogDescription")
+	val blogDescription: String,
+
+	@field:SerializedName("dateCreated")
+	val dateCreated: String,
+
+	@field:SerializedName("createdBy")
+	val createdBy: String,
+
+	@field:SerializedName("imageUrl")
+	val imageUrl: String,
+
+	@field:SerializedName("blogId")
+	val blogId: Int,
+
+	@field:SerializedName("userId")
+	val userId: String,
+
+	@field:SerializedName("blogTitle")
+	val blogTitle: String
 )
