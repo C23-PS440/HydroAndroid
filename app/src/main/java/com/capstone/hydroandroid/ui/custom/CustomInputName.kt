140ecfd9f063d38ac9bee4c8d6a69b90ad6ad8e6
cyncindy <cindydevonline@gmail.com>
2023-06-13 13:28:24 +0800

    modified edt & btn using custom edt and custom btn in register

---
+++ app/src/main/java/com/capstone/hydroandroid/ui/custom/CustomInputName.kt
@@ -0,0 +1,29 @@
+package com.capstone.hydroandroid.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class CustomInputName : AppCompatEditText, View.OnTouchListener {
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        return false
+    }
+
+}
