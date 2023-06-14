a8851c34717053acff110ea606f97294b3d181d4
cyncindy <cindydevonline@gmail.com>
2023-06-14 21:51:06 +0800

    fixing swipe refresh layout issue on blog fragment

--- app/src/main/java/com/capstone/hydroandroid/ui/blog/BlogFragment.kt
+++ app/src/main/java/com/capstone/hydroandroid/ui/blog/BlogFragment.kt
@@ -1,13 +1,10 @@
 package com.capstone.hydroandroid.ui.blog
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.capstone.hydroandroid.R
 import com.capstone.hydroandroid.adapter.BlogUserAdapter
@@ -15,38 +12,37 @@ import com.capstone.hydroandroid.data.network.EventResult
 import com.capstone.hydroandroid.databinding.FragmentBlogBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-
 class BlogFragment : Fragment(R.layout.fragment_blog) {
     private val binding: FragmentBlogBinding by viewBinding()
     private val viewModel: BlogViewModel by viewModel()
-    private val swipeRefreshLayout: SwipeRefreshLayout by viewBinding(R.id.swipe_refresh_layout)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         fetchData()
+
+        binding.swipeRefreshLayout.setOnRefreshListener {
+            fetchData()
+            binding.swipeRefreshLayout.isRefreshing = false
+        }
     }
 
     private fun fetchData() {
-        swipeRefreshLayout.setOnRefreshListener {
-            viewModel.getAllUserBlog().observe(viewLifecycleOwner) {
-                when (it) {
-                    is EventResult.Error -> {
-                        Toast.makeText(requireContext(), it.error, Toast.LENGTH_SHORT).show()
-                    }
-                    is EventResult.Loading -> {
-                    }
-                    is EventResult.Success -> {
-                        val blogUserAdapter = BlogUserAdapter(it.data.blog)
-                        val layoutManager = GridLayoutManager(requireContext(),2,
-                            GridLayoutManager.VERTICAL,false)
-                        binding.rvBlog.layoutManager = layoutManager
-                        binding.rvBlog.adapter = blogUserAdapter
-                        swipeRefreshLayout.isRefreshing = false
-                    }
+        viewModel.getAllUserBlog().observe(viewLifecycleOwner) {
+            when (it) {
+                is EventResult.Error -> {
+                    Toast.makeText(requireContext(), it.error, Toast.LENGTH_SHORT).show()
+                }
+                is EventResult.Loading -> {
+                }
+                is EventResult.Success -> {
+                    val blogUserAdapter = BlogUserAdapter(it.data.blog)
+                    val layoutManager = GridLayoutManager(requireContext(),2,
+                        GridLayoutManager.VERTICAL,false)
+                    binding.rvBlog.layoutManager = layoutManager
+                    binding.rvBlog.adapter = blogUserAdapter
                 }
             }
         }
     }
-
-
 }
